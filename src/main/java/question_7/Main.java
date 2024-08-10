7b0d09e37003304329a1ad19dc2c802f66be17e3
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2024-08-10 11:10:12 +0530

    solved all questions

--- src/main/java/question_7/Main.java
+++ src/main/java/question_7/Main.java
@@ -1,15 +1,14 @@
-package question_3;
-//3. Write a code to print employee Address detail like city state country by applying constructor injection from dependency injection of spring framework.
+package question_7;
+//7. Write a code to print employee Address detajl like city state country by applying setter injection from dependency injection of spring framework.
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("question_3/applicationContext.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("question_7/applicationContext.xml");
         Employee employee = (Employee) context.getBean("employee");
         Address address = employee.getAddress();
-        System.out.println("Employee: " + employee);
         System.out.println("City: " + address.getCity());
         System.out.println("State: " + address.getState());
         System.out.println("Country: " + address.getCountry());
